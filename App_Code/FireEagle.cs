5d06ab3bdbf99e74afbb0a763f61f9a9b21b000a
Phillip Pearson <pp@myelin.co.nz>
2008-03-25 13:55:50 +1300

    initial import - app shell

---
+++ App_Code/FireEagle.cs
@@ -0,0 +1,20 @@
+/**
+ * FireEagle OAuth+API C# bindings
+ *
+ * Copyright (C) 2007-08 Yahoo! Inc
+ *
+ */
+
+/// <summary>
+/// Summary description for FireEagle
+/// </summary>
+public class FireEagle
+{
+    public static FE_ROOT = "http://fireeagle.yahoo.net/";
+    public static FE_API_ROOT = "https://fireeagle.yahooapis.com/";
+
+	public FireEagle(string o)
+	{
+		
+	}
+}
